82e93e1ac6b585620b45936fc2966f1c47a4cd15
omar1234567 <omaraldoboni@gmail.com>
2026-03-24 11:50:06 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ import anthropic
 WATCHLIST = {
     "US": [
         "AAPL", "MSFT", "NVDA", "AMZN", "GOOGL",
-        "META", "TSLA", "SERV", "UBER", "LLY", "UPWK"
+        "META", "TSLA", "SERV", "UBER", "LLY", "UPWK", "ONDS"
     ],
     "TSX": [
         "CSU.TO", "TD.TO", "BNS.TO", "ENB.TO", "CNQ.TO",
