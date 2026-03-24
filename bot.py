be4cb8f101c032cb29e6d514db265b49a61917e5
omar1234567 <omaraldoboni@gmail.com>
2026-03-24 14:28:59 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ WATCHLIST = {
     "US": [
         # Technology
         "NVDA", "AAPL", "MSFT", "GOOGL", "AVGO",
-        "AMD", "CSCO", "MU", "LRCX", "ORCL", "TXN", "PATH",
+        "AMD", "CSCO", "MU", "LRCX", "ORCL", "TXN", "PATH","UBER",
         # Healthcare & Pharma
         "LLY", "JNJ", "ABBV", "MRK", "ABT", "TMO",
         # Consumer & Industrial
